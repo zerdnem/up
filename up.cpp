a63574bc3dba19f976cb8e713e31ae42229c6e8f
jjan <jjan@teknik.io>
2019-11-03 20:17:39 +0400

    replaced Reader with CharReaderBuilder

--- up.cpp
+++ up.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <fstream>
 
 #include <curl/curl.h>
@@ -6,16 +7,16 @@
 
 namespace
 {
-    size_t cb(
-            const char *in,
-            size_t size,
-            size_t num,
-            std::string *out)
-    {
-        const size_t totalBytes(size * num);
-        out->append(in, totalBytes);
-        return totalBytes;
-    };
+std::size_t cb(
+    const char *in,
+    std::size_t size,
+    std::size_t num,
+    char *out)
+{
+    std::string data(in, (std::size_t)size * num);
+    *((std::stringstream *)out) << data;
+    return size * num;
+}
 } // namespace
 
 struct progress
@@ -27,10 +28,10 @@ struct progress
 #define ONE_MEGABYTE (CURL_OFF_T_C(1024) * 1024)
 
 static int xferinfo(void *p,
-        curl_off_t dltotal, curl_off_t dlnow,
-        curl_off_t ultotal, curl_off_t ulnow)
+                    curl_off_t dltotal, curl_off_t dlnow,
+                    curl_off_t ultotal, curl_off_t ulnow)
 {
-    fprintf(stderr, "UP: %" CURL_FORMAT_CURL_OFF_T " of %" CURL_FORMAT_CURL_OFF_T " MB\r", ulnow/ONE_MEGABYTE, ultotal/ONE_MEGABYTE);
+    fprintf(stderr, "UP: %" CURL_FORMAT_CURL_OFF_T " of %" CURL_FORMAT_CURL_OFF_T " MB\r", ulnow / ONE_MEGABYTE, ultotal / ONE_MEGABYTE);
     return 0;
 }
 
@@ -78,9 +79,9 @@ void fileUpload(std::string genDeletionKey, std::string filename)
         curl_easy_setopt(curl, CURLOPT_URL, "https://api.teknik.io/v1/Upload");
         curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
 
-        std::string *httpData(new std::string());
+        std::stringstream httpData;
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, httpData);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &httpData);
         curl_easy_setopt(curl, CURLOPT_NOPROGRESS, false);
         curl_easy_setopt(curl, CURLOPT_XFERINFOFUNCTION, xferinfo);
         curl_easy_setopt(curl, CURLOPT_XFERINFODATA, &prog);
@@ -95,8 +96,9 @@ void fileUpload(std::string genDeletionKey, std::string filename)
         if (httpCode == 200)
         {
             Json::Value jsonData;
-            Json::Reader jsonReader;
-            if (jsonReader.parse(*httpData, jsonData))
+            Json::CharReaderBuilder jsonReader;
+            std::string errs;
+            if (Json::parseFromStream(jsonReader, httpData, &jsonData, &errs))
             {
                 const std::string url(jsonData["result"]["url"].asString());
                 const std::string deletionKey(jsonData["result"]["deletionKey"].asString());
@@ -139,7 +141,9 @@ int main(int argc, char *argv[])
             {
                 genDeletionKey = "true";
                 fileUpload(genDeletionKey, argv[2]);
-            } else {
+            }
+            else
+            {
                 std::cout << "File doesn't exists!\n";
             }
         }
@@ -154,7 +158,9 @@ int main(int argc, char *argv[])
         if (check_file_exists(arg.c_str()))
         {
             fileUpload(genDeletionKey, arg);
-        } else {
+        }
+        else
+        {
             std::cout << "File doesn't exists!\n";
         }
     }
